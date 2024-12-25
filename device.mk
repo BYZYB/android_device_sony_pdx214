e21e226d6e41b672d0decf96e99fd5647dc073bb
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    pdx214: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -260,6 +260,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    NcmTetheringOverlay \
     SonyPDX214FrameworksRes \
     SonyPDX214SystemUIRes \
     SonySagamiFrameworksResCommon \
