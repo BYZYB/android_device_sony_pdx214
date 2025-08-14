46ce9b4962f4e661703dfcb83a7e7c43571ff7c3
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    pdx214: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.sony
+    vendor.lineage.livedisplay-service.sony
 
 # Media (Device)
 PRODUCT_COPY_FILES += \
